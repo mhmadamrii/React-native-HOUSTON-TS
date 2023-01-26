58accaa81d7d437a5db2ba8f89a2d4c8d542ffd6
mhmadamrii <interceptorghost4@gmail.com>
2023-01-26 16:31:08 +0700

    add task and desc textinput

--- screens/Todo/index.tsx
+++ screens/Todo/index.tsx
@@ -1,36 +1,79 @@
 import React, { useState } from 'react'
-import { View, Text, FlatList, Button, TextInput, Platform } from 'react-native'
+import {
+  View,
+  Text,
+  FlatList,
+  Button,
+  TextInput,
+  Platform,
+  TouchableOpacity,
+} from 'react-native'
 
 interface Todo {
   id: string
-  task: string
+  task: string,
+  description: string,
   isComplete: boolean
 }
 
 const Todo: React.FC = () => {
   const [todos, setTodos] = useState<Todo[]>([])
   const [newTodos, setNewTodos] = useState<string>('')
+  const [description, setDescription] = useState<string>('')
 
   const handleAddTodo = () => {
     setTodos([
       ...todos,
-      { id: Date.now().toString(), task: newTodos, isComplete: false },
+      { id: Date.now().toString(), task: newTodos, isComplete: false, description },
     ])
     setNewTodos('')
+    setDescription('')
   }
 
-  console.log('newTodos', newTodos)
+  const handleCompleteTodo = (id: string) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isComplete: !todo.isComplete } : todo,
+      ),
+    )
+  }
+
+  const handleRemoveTodo = (id: string) => {
+    console.log('id?', id)
+    setTodos(todos.filter((todo) => todo.id !== id))
+  }
+
   const renderTodos = ({ item }: { item: Todo }) => {
-    console.log('items', item)
     return (
-      <View>
+      <View
+        style={{
+          flex: 1,
+          width: '80%',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          marginHorizontal: 50,
+        }}
+      >
         <Text
           style={{
             textDecorationLine: item.isComplete ? 'line-through' : 'none',
+            textAlign: 'center',
+            fontSize: 30,
           }}
         >
-          {item.task}
+          {item.task} desc: {item.description}
         </Text>
+
+        <View style={{ flexDirection: 'row' }}>
+          <TouchableOpacity
+            onPress={() => handleRemoveTodo(item.id)}
+            style={{ backgroundColor: 'red' }}
+          >
+            <Text>Delete</Text>
+          </TouchableOpacity>
+          <Button title="tes" onPress={() => handleCompleteTodo(item.id)} />
+        </View>
       </View>
     )
   }
@@ -38,15 +81,28 @@ const Todo: React.FC = () => {
   return (
     <>
       <View style={{ flex: 1, marginTop: Platform.OS == 'android' ? 30 : 0 }}>
-        <TextInput
-          style={{ borderWidth: 1, height: 80, padding: 5 }}
-          placeholder="what's going on?"
-          onChangeText={setNewTodos}
-        />
-        <Button title="add new task?" onPress={handleAddTodo} />
+        <View>
+          <TextInput
+            style={{ borderWidth: 1, height: 80, padding: 5, fontSize: 30 }}
+            placeholder="what's going on?"
+            onChangeText={setNewTodos}
+            value={newTodos}
+          />
+          <TextInput
+            style={{ borderWidth: 1, height: 80, padding: 5, fontSize: 30 }}
+            placeholder="eg: lorem ipsum dolor"
+            onChangeText={setDescription}
+            value={description}
+          />
+          <Button title="add new task?" onPress={handleAddTodo} />
+        </View>
 
         <View>
-          <FlatList data={todos} renderItem={renderTodos} keyExtractor={item => item.id} />
+          <FlatList
+            data={todos}
+            renderItem={renderTodos}
+            keyExtractor={(item) => item.id}
+          />
         </View>
       </View>
     </>
